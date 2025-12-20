545e692468210f0c23072ada73b164f9fa225bfe
yama <yamamotoheihei0999@gmail.com>
2025-12-21 00:16:09 +0900

    feat(hero): responsive picture with breakpoint sources (1024/1600)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import hero from '../assets/hero.svg'
 
 export default function Hero() {
   return (
     <section className="hero container" aria-hidden="false">
-      <img src={hero} alt="メインビジュアル" />
+      <picture>
+        <source media="(min-width:1200px)" srcSet="/images/photo-1-1600.svg" />
+        <source media="(min-width:768px)" srcSet="/images/photo-1-1024.svg" />
+        <img src="/images/photo-1.jpg" alt="メインビジュアル" loading="lazy" />
+      </picture>
     </section>
   )
 }
