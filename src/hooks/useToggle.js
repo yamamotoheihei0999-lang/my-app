c5560c5982e3e07487a5a0d35965dc50cbf42ff0
yama <yamamotoheihei0999@gmail.com>
2025-12-21 12:30:02 +0900

    chore: 未使用のフォルダとファイルを削除してプロジェクト構成を整理

--- src/hooks/useToggle.js
+++
@@ -1,7 +0,0 @@
-import { useState, useCallback } from 'react'
-
-export default function useToggle(initial = false) {
-  const [state, setState] = useState(initial)
-  const toggle = useCallback(() => setState((s) => !s), [])
-  return [state, toggle, setState]
-}
--- src/services/api.js
+++
@@ -1,5 +0,0 @@
-export async function fetchJSON(url, options) {
-  const res = await fetch(url, options)
-  if (!res.ok) throw new Error(res.statusText)
-  return res.json()
-}
